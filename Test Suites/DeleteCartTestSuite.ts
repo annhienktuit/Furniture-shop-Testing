4b5a7a976b67dd3725c77d9f8c252d888c5da827
unknown <annhien.se@gmail.com>
2021-12-15 22:05:00 +0700

    feat: update some content

--- Test Suites/DeleteCartTestSuite.ts
+++ Test Suites/DeleteCartTestSuite.ts
@@ -5,10 +5,16 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>eeeb7420-062a-4ed5-8cff-cdd2be4db47f</testSuiteGuid>
+   <testCaseLink>
+      <guid>c78ba788-6be5-47be-9b44-a43df6dcca8b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Delete Cart/RemoveOneProductFromCart</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
